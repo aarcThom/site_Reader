a674e099451c7b03facc1c72cf8d9f63e140c346
Thomas <Thomas@TG_LAPTOP>
2023-08-09 23:13:36 -0700

    added header info component

---
+++ siteReader/Components/HeaderInfo.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using siteReader.Methods;
+using siteReader.Params;
+
+namespace siteReader.Components
+{
+    public class HeaderInfo : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the MyComponent1 class.
+        /// </summary>
+        public HeaderInfo()
+          : base("LAS header info", "header",
+            "Read a LAS file's header info",
+            "AARC", "siteReader")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddParameter(new AsprParam(), "ASPR Cloud", "cld", "A point cloud linked with ASPRS data", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("Header", "header", "Header information.", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            AsprCld cld = new AsprCld();
+            if (!DA.GetData(0, ref cld)) return;
+
+            List<string> header = Utility.FloatDictGhOut(cld.header, this);
+
+            DA.SetDataList(0, header);
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("2244C6D1-85AC-4E0C-A4CB-55549DB009BD"); }
+        }
+    }
+}
